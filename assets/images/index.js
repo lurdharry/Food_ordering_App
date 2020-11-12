48e59663b5bc00fb97331276114b88f49dcb42ea
lurdharry <azeezhammed57@gmail.com>
2020-11-13 00:45:21 +0100

    Added new images and icons

--- assets/images/index.js
+++ assets/images/index.js
@@ -1,3 +1,4 @@
 export const onboard1 = require('./onboard1.png');
 export const onboard2 = require('./onboard2.png');
 export const onboard3 = require('./onboard3.png');
+export const RestImage = require('./rest.png');
